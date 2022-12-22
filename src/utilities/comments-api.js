7d2ac041dc07aae2bb40dc39e40b0e2e3cb1a041
pinafern <pinafern4@mgail.com>
2022-12-21 16:59:14 -0800

    Deploy

--- src/utilities/comments-api.js
+++ src/utilities/comments-api.js
@@ -5,8 +5,8 @@ export async function index(jersey) {
   return await sendRequest(`/api/jerseys/${jersey._id}`);
 }
 
-export async function createComment(commentData, donut) {
-  return sendRequest(`${BASE_URL}/${donut._id}/new`, 'POST', commentData);
+export async function createComment(commentData, jersey) {
+  return sendRequest(`${BASE_URL}/${jersey._id}/new`, 'POST', commentData);
 }
 
 export async function updateComment(commentFormData, id) {
